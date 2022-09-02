c991041ae10502bf31c6c04f331978ce2c85a0cd
Erdij <yildirimsukruerdi@gmail.com>
2022-09-02 04:56:44 +0300

    Task_Comp

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -2,7 +2,31 @@ import React from "react";
 
 class MovieList extends React.Component {
   render() {
-    return <h1>Movie List</h1>;
+    return (
+      <div className="row">
+        <div className="col-lg-4">
+          <div className="card mb-4 shadow-sm ">
+            <img
+              src="https://image.tmdb.org/t/p/w600_and_h900_bestv2/dXNAPwY7VrqMAo51EKhhCJfaGb5.jpg"
+              className="card-img-top"
+              alt="Sample Text"
+            />
+            <div className="card-body">
+              <h5 className="card-title">Sample Movie</h5>
+              <p className="card-text">Sample Movie Description </p>
+              <div className="d-flex justify-content-between align-items-center ">
+                <button type="button" className="btn btn-md btn-outline-danger">
+                  Delete
+                </button>
+                <h2>
+                  <span className="">9.0</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
